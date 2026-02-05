bffa03b145aa6140899390d3fbe7f1d6b47f4aad
Gowtham Rayadurgam <gowtham_rayadurgam@epam.com>
2026-02-05 19:26:18 +0530

    new pipelines added

--- backend/functionapp/Program.cs
+++ backend/functionapp/Program.cs
@@ -1,14 +1,7 @@
-using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Azure.Functions.Worker.AspNetCore;
 
-var builder = WebApplication.CreateBuilder(args);
+var host = Host.CreateDefaultBuilder(args)
+	.ConfigureFunctionsWorkerDefaults()
+	.Build();
 
-builder.Services.AddFunctionsWorkerDefaults();
-
-var app = builder.Build();
-
-app.ConfigureFunctionsWebApplication();
-
-app.Run();
+host.Run();
--- backend/functionapp/obj/Release/net6.0/TestFunctionApp.AssemblyInfo.cs
+++ backend/functionapp/obj/Release/net6.0/TestFunctionApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestFunctionApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0ba689dfcc7690f1606e036408dc82ea90e4436")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+454386bb06a26fa8b8c0b1234e5c0c9d06e086d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestFunctionApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestFunctionApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
