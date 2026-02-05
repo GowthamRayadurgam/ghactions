454386bb06a26fa8b8c0b1234e5c0c9d06e086d6
Gowtham Rayadurgam <gowtham_rayadurgam@epam.com>
2026-02-05 19:21:33 +0530

    new pipelines added

--- backend/functionapp/Program.cs
+++ backend/functionapp/Program.cs
@@ -1,8 +1,14 @@
-using Microsoft.Azure.Functions.Worker;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Azure.Functions.Worker.AspNetCore;
 
-var host = new HostBuilder()
-    .ConfigureFunctionsWorkerDefaults()
-    .Build();
+var builder = WebApplication.CreateBuilder(args);
 
-host.Run();
+builder.Services.AddFunctionsWorkerDefaults();
+
+var app = builder.Build();
+
+app.ConfigureFunctionsWebApplication();
+
+app.Run();
